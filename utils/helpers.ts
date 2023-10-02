44a207f40da16863b87c82cbd879885870dcf5ca
gaurav.chopda@techholding.co <gauravrchopada65@gmail.com>
2023-10-02 18:17:07 +0530

    v2: added email integration

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,4 +1,6 @@
-function convertToSubarrays<T>(data: T[]): T[][] {
+'use client'
+
+export function convertToSubarrays<T>(data: T[]): T[][] {
     const subarrays: T[][] = [];
     for (let i = 0; i < data.length; i += 3) {
       const subarray = data.slice(i, i + 3);
@@ -7,4 +9,17 @@ function convertToSubarrays<T>(data: T[]): T[][] {
     return subarrays;
 }
 
-export {convertToSubarrays}
+export async function sendMail({name, email, subject, message}: {name: string, email: string, subject: string, message: string}): Promise<boolean> {
+  const init = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({name, email, subject, message}),
+  }
+
+  const response = await fetch('https://gaurav-chopda-portfolio-server.vercel.app/send',init )
+  return response.status === 200
+}
